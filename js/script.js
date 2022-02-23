8a4a1a2a81d8a1ebe43e9231d65fd3b502b4ca09
Dan Rogers <danielneilrogers@gmail.com>
2022-02-23 16:46:12 -0500

    2-23-2022_Commit
    completed array and getRandomQuote function

--- js/script.js
+++ js/script.js
@@ -8,15 +8,29 @@ project 1 - A Random Quote Generator
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
- * `quotes` array 
+ * `quotes` array: arrary of objects storing quotes and corresponding data 
 ***/
+let quotes = [
+  {quote: "The journey of a thousand miles begins with one step.", source: "Lao Tzu", citation: "https://www.keepinspiring.me/famous-quotes/", year: "unknown" },
+  {quote: "That which does not kill us makes us stronger", source: "Friedrich Nietzsche", citation: "https://www.keepinspiring.me/famous-quotes/", year: "1888" },
+  {quote: "Life is what happens when you’re busy making other plans.", source: "John Lennon", citation: "https://www.keepinspiring.me/famous-quotes/", year: "1957" },
+  {quote: "When the going gets tough, the tough get going.", source: "Joe Kennedy", citation: "https://www.keepinspiring.me/famous-quotes/", year: "" },
+  {quote: "You must be the change you wish to see in the world.", source: "Mahatma Gandhi", citation: "https://www.keepinspiring.me/famous-quotes/", year: "" }
+];
 
-
+// console.log(quotes);
+// console.log(quotes.length);
 
 /***
- * `getRandomQuote` function
+ * `getRandomQuote` function: creates random number based on the quotes array length. Then returns the object selected.
 ***/
+function getRandomQuote(arr) {
+  let quoteIndex = Math.floor(Math.random() * arr.length);
+  let quoteSelectObject = arr[quoteIndex];
+  return quoteSelectObject;
+};
 
+//console.log(getRandomQuote(quotes));
 
 
 /***
@@ -30,4 +44,4 @@ project 1 - A Random Quote Generator
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+// document.getElementById('load-quote').addEventListener("click", printQuote, false);
